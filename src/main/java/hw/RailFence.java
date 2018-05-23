36bbfb1c01699c59aa22c886ca9ab128ee7c5683
Ben Lorentz <blorentz@luc.edu>
2018-05-23 16:54:17 -0500

    Railfence works

--- src/main/java/hw/RailFence.java
+++ src/main/java/hw/RailFence.java
@@ -30,24 +30,41 @@ public class RailFence {
 
     System.out.println("What is the string you want to encrypt?");
     String input = keyboard.nextLine();
+    // String formating ie. lowercase no spaces
     String phrase = input.toLowerCase();
     phrase.replaceAll(" ", "");
 
     System.out.println("What is the number of rails you want to create?");
     int rails = keyboard.nextInt();
 
+    // stringbuilder to assemble final string
     StringBuilder inProg = new StringBuilder("");
     String result = "";
 
-    char[] phraseArr = phrase.toCharArray();
-    System.out.println(phraseArr);
-    for (int r = 0; r < rails; r++) {
-      for (int let = 0; let < phraseArr.length; let++) {
-        int checkVal = ((2 * rails) - 2);
-        if (let % checkVal == 0) {
-          System.out.println(let);
-          inProg.append(phraseArr[let]);
-        }
+    // the board array and nessecary variables
+    int textLength = phrase.length();
+    char[][] board = new char[rails][textLength];
+    boolean down = false;
+    int count = 0;
+
+    // moves through the matrix and moves up and down based on the position
+    for (int i = 0; i < textLength; i++) {
+
+      if (count == 0 || count == (rails - 1)) {
+        down = !down;
+      }
+
+      board[count][i] = phrase.charAt(i);
+      if (down == true) {
+        count++;
+      } else {
+        count--;
+      }
+    }
+    // passed throught the array normally and forms the cipher text
+    for (int k = 0; k < rails; k++) {
+      for (int l = 0; l < textLength; l++) {
+        inProg.append(board[k][l]);
       }
     }
 
@@ -56,6 +73,7 @@ public class RailFence {
   }
 
   public void decrypt() {
+    // nessecary parameters from the user
     System.out.println("What is the string you want to decrypt?");
     String input = keyboard.nextLine();
     String phrase = input.toLowerCase();
@@ -63,9 +81,52 @@ public class RailFence {
     System.out.println("What is the number of rails you want to use?");
     int rails = keyboard.nextInt();
 
-    StringBuilder inProg = new StringBuilder("");
+    StringBuilder inProgD = new StringBuilder("");
     String result = "";
 
-    for (char character : phrase.toCharArray()) {}
+    int textLength = phrase.length();
+    char[][] board = new char[rails][textLength];
+    int count = 0;
+    boolean down = false;
+    int place = 0;
+
+    for (int i = 0; i < textLength; i++) {
+
+      if (count == 0 || count == (rails - 1)) {
+        down = !down;
+      }
+      board[count][i] = '*';
+      if (down == true) {
+        count++;
+      } else {
+        count--;
+      }
+    }
+
+    for (int i = 0; i < rails; i++) {
+      for (int j = 0; j < textLength; j++) {
+        if (board[i][j] == '*' && place < textLength) {
+          board[i][j] = phrase.charAt(place++);
+        }
+      }
+    }
+
+    count = 0;
+    down = false;
+    for (int i = 0; i < textLength; i++) {
+
+      if (count == 0 || count == (rails - 1)) {
+        down = !down;
+      }
+      inProgD.append(board[count][i]);
+      if (down == true) {
+        count++;
+      } else {
+        count--;
+      }
+    }
+
+    result = inProgD.toString();
+    System.out.println("Your result is: " + result);
   }
 }
